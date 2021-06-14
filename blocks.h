ac9b307b44e3753a581557a309d37a5fa0c06670
BajacDev <baptiste.jacquemot@orange.fr>
2021-06-14 11:45:36 +0200

    update more often

--- blocks.h
+++ blocks.h
@@ -1,15 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "",	0,		0},
+	//{"", "nettraf",	1,		0},
 	{"", "backlight",	0,		11},
 	{"", "volume",	0,		10},
-	{"", "internet",	5,		0},
-	{"", "battery",	30,		0},
+	{"", "internet",	1,		0},
+	{"", "battery",	1,		0},
 
-	{"", "date '+%b %d (%a) %H:%M'",					5,		0},
+	{"", "clock",					1,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " ❘ ";
 static unsigned int delimLen = 5;
